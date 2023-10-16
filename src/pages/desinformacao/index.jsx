87bce90d7e7aeeab9b01f0fe18c1b7c8b0302396
Arthur Felipe Schmitz <arthurfschmitz22@gmail.com>
2023-10-15 23:13:29 -0300

    create desinfo page and fetching api

--- src/pages/desinformacao/index.jsx
+++ src/pages/desinformacao/index.jsx
@@ -1,6 +1,46 @@
+import useSWR from "swr";
+import { Container, Row, Title } from "./styles";
 
 export default function Desinformacao() {
+  const { data: videos, error } = useSWR("http://127.0.0.1:8000/api/podcast-desinfo",async (url) => {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    }
+  );
+
+  if (error) {
+    return <div>Opss.. {error.message}</div>;
+  }
+
+  if (!videos) {
+    return <div>Carregando ...</div>;
+  }
+
   return (
-    <h1>deu boa kkk</h1>
-  )
+    <Container>
+      <Title>
+        <h1>Podcast <span>desinformação</span></h1>
+        <a href="/">Voltar</a>
+      </Title>
+      <ul>
+        {videos.map((video) => (
+          <li key={video.id}>
+            <Row>
+              <h2>{video.title}</h2>
+              <iframe
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${video.video_id}`}
+                title={video.title}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+            </Row>
+          </li>
+        ))}
+      </ul>
+    </Container>
+  );
 }
--- src/pages/desinformacao/styles.js
+++ src/pages/desinformacao/styles.js
@@ -1,1 +1,49 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 100%;
+  background-color: #141414;
+`;
+
+export const Title = styled.div`
+  background-color: #000;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  h1 {
+    padding: 50px;
+    color: #fff;
+  }
+
+  span {
+    color: #FFC739;
+  }
+
+  a{
+    color: #fff;
+    padding-right: 100px;
+    text-decoration: none;
+    font-size: 20px;
+  }
+`;
+
+export const Row = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  flex-wrap: wrap;
+  padding: 50px;
+
+  h2 {
+    margin: 10px;
+    text-transform: uppercase;
+    color: #fff;
+  }
+
+  iframe {
+
+  }
+`;
