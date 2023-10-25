a4324141e5ab3daaba7fcbfc40e96ad5abb52c36
Arthur Felipe Schmitz <arthurfschmitz22@gmail.com>
2023-10-24 22:28:37 -0300

    upp

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    API_KEY: 'https://podcast-desinfo-api-production.up.railway.app/api'
+  },
 }
 
 module.exports = nextConfig
